8c9e70e682248a9132b4de0946caa2b797d2869d
Joathan Souza <joathanf@gmail.com>
2021-12-14 03:39:33 -0300

    feat: :sparkles: criando rotas para ShortUrl

---
+++ src/router.ts
@@ -0,0 +1,28 @@
+import { createWebHistory, createRouter } from "vue-router";
+import { RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    alias: "/short_urls",
+    name: "short_urls",
+    component: () => import("./components/short_url/ListShortUrl.vue"),
+  },
+  {
+    path: "/add",
+    name: "add",
+    component: () => import("./components/short_url/AddShortUrl.vue"),
+  },
+  {
+    path: "/top100",
+    name: "top100",
+    component: () => import("./components/short_url/TopHundred.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
